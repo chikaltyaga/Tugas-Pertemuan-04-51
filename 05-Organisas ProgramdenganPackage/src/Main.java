e0613e700afd2025db4c4d758b063653e545fd39
chikaltyaga <55616548+chikaltyaga@users.noreply.github.com>
2019-12-20 23:21:33 +0700

    Add files via upload

---
+++ 05-Organisas ProgramdenganPackage/src/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author STMIK SUMEDANG
+ * Nama     :Chikal Tyaga
+ * NIM      :A2.1900030
+ * Kelas    :TI-1C
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("hello world");
+    }
+    
+}
---
+++ 07-MemahamiString[]args/src/com/tutorial/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tutorial;
+
+/**
+ *
+ * @author STMIK SUMEDANG
+ * Nama     :Chikal Tyaga
+ * NIM      :A2.1900030
+ * Kelas    :TI-1C
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("hallo " + "ganteng");
+        System.out.println("hallo " + "Manise");
+    }
+    
+}
